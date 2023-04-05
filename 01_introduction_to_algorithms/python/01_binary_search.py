f3fc02fe7f498f04cd18edf9ba17877ad2cca215
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-04-05 10:14:37 +0000

    Add docstrings and fix print statement

--- 01_introduction_to_algorithms/python/01_binary_search.py
+++ 01_introduction_to_algorithms/python/01_binary_search.py
@@ -1,28 +1,36 @@
-def binary_search(list, item):
-  # low and high keep track of which part of the list you'll search in.
-  low = 0
-  high = len(list) - 1
+'''
+Chapter 1: Introduction to Algorithms
+'''
 
-  # While you haven't narrowed it down to one element ...
-  while low <= high:
-    # ... check the middle element
-    mid = (low + high) // 2
-    guess = list[mid]
-    # Found the item.
-    if guess == item:
-      return mid
-    # The guess was too high.
-    if guess > item:
-      high = mid - 1
-    # The guess was too low.
-    else:
-      low = mid + 1
 
-  # Item doesn't exist
-  return None
+def binary_search(items, item):
+    '''Returns the index of the item if found, or None if not found.'''
+
+    # low and high keep track of which part of the list you'll search in.
+    low = 0
+    high = len(items) - 1
+
+    # While you haven't narrowed it down to one element ...
+    while low <= high:
+        # ... check the middle element
+        mid = (low + high) // 2
+        guess = items[mid]
+        # Found the item.
+        if guess == item:
+            return mid
+        # The guess was too high.
+        if guess > item:
+            high = mid - 1
+        # The guess was too low.
+        else:
+            low = mid + 1
+
+    # Item doesn't exist
+    return None
+
 
 my_list = [1, 3, 5, 7, 9]
-print binary_search(my_list, 3) # => 1
+print(binary_search(my_list, 3))  # => 1
 
 # 'None' means nil in Python. We use to indicate that the item wasn't found.
-print binary_search(my_list, -1) # => None
+print(binary_search(my_list, -1))  # => None
